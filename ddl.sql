699a22140dcc0a3b1a6194fb22fb32ab38b00004
kunxin-chor <chorkunxin@yahoo.com>
2021-10-15 08:02:20 +0000

    Added ddl for create database

---
+++ ddl.sql
@@ -0,0 +1,11 @@
+/* show all databases on server */
+show databases;
+
+/* create new database */
+create database swimming_school;
+
+/* switch to a database (i.e make it active )*/
+use swimming_school;
+
+/* see the current database */
+SELECT DATABASE();
