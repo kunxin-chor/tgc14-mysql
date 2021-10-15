4859b2af78c9f48bc7482e7fc0b69ff649be2a4e
kunxin-chor <chorkunxin@yahoo.com>
2021-10-15 09:35:40 +0000

    Added FK explanation

--- ddl.sql
+++ ddl.sql
@@ -45,3 +45,38 @@ update parents set last_name="Patterson" where parent_id=4;
 /* Delete */
 /* Remove the parent with the id 3 */
 delete from parents where parent_id = 3;
+
+/* students table */
+create table students (
+    student_id int unsigned primary key auto_increment,
+    name varchar(100) not null,
+    date_of_birth date not null,
+    swimming_level tinyint not null
+) engine=innodb;
+
+/* add the foreign key */
+alter table students add parent_id int unsigned not null;
+/* for every row in the students table,
+each row's parent_id must refer to a valid parent_id in the parents table */
+alter table students add constraint fk_students_parents
+    foreign key (parent_id) references parents(parent_id);
+
+insert into students (name, date_of_birth, swimming_level, parent_id)
+ values ('Litle Dog', '1999-09-08', 1, 2);
+
+/* the below won't work */
+ insert into students(name, date_of_birth, swimming_level, parent_id)
+ values("Naughty Boy", "1998-09-10", 1,99 );
+
+ create table swimming_levels (
+     level_id int unsigned auto_increment primary key,
+     name varchar(200)
+ )engine=innodb;
+
+/* add to the student table a new column  named level_id */
+ alter table students add level_id int unsigned not null;
+
+/* we cannot add the new FK because there are existing students
+ which level_id is 0 unless we delete all the students first*/
+ alter table students add constraint fk_students_swimming_levels
+    foreign key (level_id) references swimming_levels(level_id);
