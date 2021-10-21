63cc06229c12494ced1afe7325acd4bb74cdabfb
kunxin-chor <chorkunxin@yahoo.com>
2021-10-21 08:16:52 +0000

    Addd CRUD for strong entity

--- index.js
+++ index.js
@@ -59,6 +59,56 @@ async function main(){
     app.get('/actors/create', function(req,res){
         res.render("actor_create");
     })
+
+    app.post('/actors/create', async function(req,res){
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        // prepared query or prepared statement
+        let query = `INSERT INTO actor (first_name, last_name)
+                             VALUES (?, ?);`;
+        let bindings = [firstName, lastName];   
+        await connection.execute(query, bindings);
+        res.redirect('/actors');
+    })
+
+    app.get('/actors/:actor_id/update', async function(req,res){
+        let actorId = req.params.actor_id;
+        let query = "SELECT * from actor WHERE actor_id = ?";
+        let binding = [actorId];
+        let [actors] = await connection.execute(query, binding);
+        let actor = actors[0];
+        res.render('actor_edit',{
+            'actor': actor
+        })
+
+    })
+
+    app.post('/actors/:actor_id/update', async function(req,res){
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        let actorId = req.params.actor_id;
+        let query = `UPDATE actor SET first_name = ?, last_name = ? 
+                     WHERE actor_id = ?`;
+        let bindings = [firstName, lastName, actorId ];
+        await connection.execute(query, bindings);
+        res.redirect('/actors');
+    })
+
+    app.get('/actors/:actor_id/delete', async function(req,res){
+        let query = "SELECT * FROM actor where actor_id = ?";
+        let binding = [req.params.actor_id];
+        let [actors] = await connection.execute(query, binding);
+        res.render("actor_delete", {
+            'actor': actors[0]
+        })
+    })
+
+    app.post('/actors/:actor_id/delete', async function(req,res){
+        let query = "DELETE FROM actor WHERE actor_id = ?";
+        let binding = [req.params.actor_id];
+        await connection.execute(query, binding);
+        res.redirect('/actors');
+    })
 }
 main();
 
